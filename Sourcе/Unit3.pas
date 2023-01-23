b40773dff5304155b70218462be2e9bc2499d0e0
MaXYXaM <99028243+MaXYXaM@users.noreply.github.com>
2023-01-23 09:30:10 +0300

    Update Unit3.pas

--- Sourcе/Unit3.pas
+++ Sourcе/Unit3.pas
@@ -32,30 +32,30 @@ uses unit1,unit2,unit4;
 {$R *.dfm}
 //По нажатию кнопки "Загрузить свою картинку" программа открывает диалоговое окно выбора картинки
 procedure TForm3.Button1Click(Sender: TObject);
-begin
-opendialog1.InitialDir:=Path+'\Image';
-opendialog1.Execute;
-   if OpenDialog1.FileName<> '' then
-     begin
-       Kart.LoadFromFile(OpenDialog1.FileName);
-       Orig.Canvas.StretchDraw(Rect(0,0,480,320),Kart);//И растягивает картинку до размеров 480х320
-       Puzzle;
-       form1.show;
-       form3.visible:=false;
-     end;
+  begin
+  opendialog1.InitialDir:=Path+'\Image';
+  opendialog1.Execute;
+     if OpenDialog1.FileName<> '' then
+       begin
+         Kart.LoadFromFile(OpenDialog1.FileName);
+         Orig.Canvas.StretchDraw(Rect(0,0,480,320),Kart);//И растягивает картинку до размеров 480х320
+         Puzzle;
+         form1.show;
+         form3.visible:=false;
+       end;
 
-end;
-//Кнопка "Сложность" открывает form 4(форма с выбором уровней сложности) и скрывает form3
-procedure TForm3.Button2Click(Sender: TObject);
-begin
-  form4.show;
-  form3.visible:=false;
-end;
-//Кнопка "Сложность" открывает form 2(форма с выбором уровней сложности) и скрывает form3
-procedure TForm3.Button3Click(Sender: TObject);
-begin
-  form2.show;
-  form3.visible:=false;
-end;
+  end;
+  //Кнопка "Сложность" открывает form 4(форма с выбором уровней сложности) и скрывает form3
+  procedure TForm3.Button2Click(Sender: TObject);
+  begin
+    form4.show;
+    form3.visible:=false;
+  end;
+  //Кнопка "Сложность" открывает form 2(форма с выбором уровней сложности) и скрывает form3
+  procedure TForm3.Button3Click(Sender: TObject);
+  begin
+    form2.show;
+    form3.visible:=false;
+  end;
 
 end.
